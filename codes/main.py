d9b0e93c213da8efc69ea296c2eb8f4fb149254a
soso シ <80224791+disco-io@users.noreply.github.com>
2022-08-04 14:37:12 -0400

    Add >sweet command option

--- codes/main.py
+++ codes/main.py
@@ -2,20 +2,22 @@ import discord
 import random
 import os
 
+# ----------------------------------------------------------------------------------------------------
+
 bread_count = 0
 meat_count = 0
 cheese_count = 0
 veggie_count = 0
 spread_count = 0
-
 reactions = ['🍞', '🥩', '🧀', '🥬', '🍅']
 
 client = discord.Client()
 
-#fillings menu
-bread = [ 'artisan roll', 'bagel', 'baguette', 'biscuit', 'brioche', 'brown bread', 'challah', 'ciabatta', 'cornbread', 'croissant', 'english muffin', 'focaccia', 'grissini', 'hawaiian roll', 'potato bread', 'rye bread', 'multi-grain bread', 'pumpernickel', 'smoked salmon', 'sourdough bread', 'white bread', 'whole wheat bread']
+# ----------------------------------------------------------------------------------------------------
 
-meat = ['bacon', 'black forest ham', 'bologna sausage', 'buffalo chicken', 'grilled chicken breast', 'honey ham', 'meatballs', 'pepperoni', 'pulled pork' 'roast beef', 'shredded chicken''steak', 'tuna', 'tuna', 'turkey salami','turkey breast']
+bread = [ 'artisan roll', 'bagel', 'baguette', 'biscuit', 'brioche', 'brown bread', 'challah', 'ciabatta', 'cornbread', 'croissant', 'english muffin', 'focaccia', 'grissini', 'hawaiian roll', 'hoagie roll', 'potato bread', 'rye bread', 'multi-grain bread', 'pumpernickel', 'smoked salmon', 'sourdough bread', 'white bread', 'whole wheat bread']
+
+meat = ['bacon', 'black forest ham', 'bologna sausage', 'buffalo chicken', 'cha lua', 'grilled chicken breast', 'honey ham', 'meatballs', 'pepperoni', 'pulled pork' 'roast beef', 'shredded chicken', 'steak', 'tuna', 'tuna', 'turkey salami','turkey breast']
 
 cheese = ['american cheese', 'brie', 'cheddar', 'gouda', 'havarti', 'parmesan', 'pepper jack', 'provolone', 'swiss', 'mozzarella']
 
@@ -23,7 +25,23 @@ veggie = ['basil', 'coleslaw', 'diced celery', 'jalepeno' 'pickles', 'lettuce', 
 
 spreads = ['blue cheese dressing', 'guacamole', 'honey mustard', 'hot sauce', 'hummus', 'mayonnaise', 'mustard', 'ranch', 'smoky bbq', 'spicy mayo', 'sweet onion dip', 'ketchup']
 
-#in the kitchen
+sweet = ['**peanut butter & jelly sandwich**:\n\
+:bread: white bread\n:grapes: grape jelly\n:peanuts: peanut butter',
+        '**strawberry sando**:\n\
+:bread: shokupan (japanese milk bread), no crust\n:icecream: whipped cream\n:strawberry: strawberries\n:dagger: slice diagonally', 
+        '**fruit sando**:\n\
+:bread: shokupan (japanese milk bread), no crust\n:icecream: whipped cream\n:kiwi: strawberry, orange, kiwi\n:dagger: slice diagonally',
+        '**the elvis**:\n\
+:bread: white bread\n:banana: toasted banana slices\n:peanuts: peanut butter\n:bacon: bacon\n:dagger: slice diagonally',
+        '**raspberry marshmallow**:\n\
+:bread: toasted sourdough bread\n:strawberry: raspberries\n:lollipop: melted white chocolate & marshmallow\n:coconut: coconut oil',       
+        '**peanut butter-nutella**:\n\
+:bread: white bread\n:strawberry: strawberries, banana slices\n:chocolate_bar: nutella\n:peanuts: peanut butter',
+        '**chocolate french toast**:\n\
+:bread: cinnamon french toast\n:banana: banana slices\n:chocolate_bar: nutella\n:honey_pot: maple syrup\n:salt: powdered sugar']
+
+# ----------------------------------------------------------------------------------------------------
+
 def choose_bread():
   return (':bread: **bread:** ' + random.choice(bread))
 
@@ -45,33 +63,49 @@ def make_sandwich():
           + choose_cheese() + '\n' \
           + choose_veggie() + '\n' \
           + choose_spread())
+
+def make_sweet():
+  return (random.choice(sweet))
   
-#online message
+# ----------------------------------------------------------------------------------------------------
+
 @client.event
 async def on_ready():
   print('{0.user}'.format(client) + ' is now up and running! ヾ(^ ∇ ^)ノ') 
+
+# ----------------------------------------------------------------------------------------------------
 
 @client.event
 async def on_message(message):
   global bread_count, meat_count, cheese_count, veggie_count, spread_count
   
-  #if send message by bot, do nothing
   if message.author == client.user:
     return
     
-  # user-botto conversations
+# ----------------------------------------------------------------------------------------------------
+    
   if message.content.startswith('>hello'):
     await message.channel.send('( ´ ▽ `)ﾉ  heyo!')
+    
   if message.content.startswith('>how are you?'):
     await message.channel.send('(´﹃ ` ) hungry for some sandwiches。。。 :sandwich:')
+    
   if message.content.startswith('>friend'):
     await message.channel.send('( ˶ᵔ ᵕ ᵔ˶):cherry_blossom: we are already great friends!')
+    
+  if message.content.startswith('>pizza'):
+    await message.channel.send('( ⋋_⋌ ):anger: how offensive 。。。')
+    
+  if message.content.startswith('>burger'):
+    await message.channel.send("( ⋋_⋌ ):anger: we're healhier ya know 。。。")
 
-  #sandwich commands
+# ----------------------------------------------------------------------------------------------------
+    
   order_count = 0
   if message.content.startswith('>sandwich'):
     order_count = order_count + 1
-    reaction_msg = await message.channel.send('sandwich botto at your service! ( ´ ▽ `)7 \nplease react to select your preferred type of fillings & quantity.')
+    reaction_msg = await message.channel.send('sandwich botto at your service! ( ´ ▽ `)7 \
+    \nplease react to select your preferred type of fillings & quantity.')
     if order_count > 0:
       bread_count = 0
       meat_count = 0
@@ -82,14 +116,13 @@ async def on_message(message):
       await reaction_msg.add_reaction(emoji)
 
   if message.content.startswith('>randomize'):
-    await message.channel.send(":sandwich: here's your random sandwich!\n \n" + make_sandwich())
+    await message.channel.send(":sandwich: here's your ``random`` sandwich!\n \n" + make_sandwich())
+    
+  if message.content.startswith('>sweet'):
+    await message.channel.send(":sandwich: here's your ``sweet`` sandwich!\n \n" + make_sweet())
 
-  #angry sandwich
-  if message.content.startswith('>pizza'):
-    await message.channel.send('( ⋋_⋌ ):anger: how offensive 。。。')
-  if message.content.startswith('>burger'):
-    await message.channel.send("( ⋋_⋌ ):anger: we're healhier ya know 。。。")
-      
+# ----------------------------------------------------------------------------------------------------
+
 @client.event
 async def on_reaction_add(reaction, user):
   global bread_count, meat_count, cheese_count, veggie_count, spread_count
@@ -109,7 +142,7 @@ async def on_reaction_add(reaction, user):
     if cheese_count == 0:
       cheese_count = 1
     else:
-      await channel.send(choose_bread())
+      await channel.send(choose_cheese())
   if 'react to select' in reaction.message.content and reaction.emoji == '🥬':
     if veggie_count == 0:
       veggie_count = 1
@@ -120,5 +153,9 @@ async def on_reaction_add(reaction, user):
       spread_count = 1
     else:
       await channel.send(choose_spread())
-    
+
+# ----------------------------------------------------------------------------------------------------
+
 client.run(os.environ['TOKEN'])
+
+# ----------------------------------------------------------------------------------------------------
